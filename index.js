dc88633ecadadf15e78d80a2b6d64b9d63c9291a
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-02-16 01:24:32 -0500

    Complete JavaScript Event Listeners Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+/*
 function addingEventListener() {
+    alert('I was clicked!')
+}
+
+input.addEventListener('click', addingEventListener); */
+
+const input = document.querySelector('input');
+
+function addingEventListener() {
+  input.addEventListener('click', function() {
+    return "clicked"
+  });
 }
